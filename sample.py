f5b346f9015dff69c8543df35d0af42bea427186
Sashakras1986 <39858629+Sashakras1986@users.noreply.github.com>
2020-11-04 21:57:17 +0300

    readme updated

--- sample.py
+++ sample.py
@@ -1,21 +1,5 @@
-import time
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium import webdriver
+s = 'My Name is Julia'
 
-try:
-    driver = webdriver.Chrome()
-    driver.get("http://suninjuly.github.io/wait2.html")
-    # говорим Selenium проверять в течение 5 секунд, пока кнопка не станет кликабельной
-    button = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, "verify")))
-    button.click()
-    message = driver.find_element_by_id("verify_message")
-
-    assert "successful" in message.text
-
-finally:
-    # ожидание чтобы визуально оценить результаты прохождения скрипта
-    time.sleep(3)
-    # закрываем браузер после всех манипуляций
-    driver.quit()
+index = s.find('Name')
+if index != -1:
+    print(f'Substring found at index {index}')
